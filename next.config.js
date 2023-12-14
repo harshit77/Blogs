2f112e5bd40900357cd956cf18894c1875e1f753
Harshit Sahu <hsahu77@gmail.com>
2023-12-14 23:00:50 +0530

    Reverted the code

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode:false,
     webpack:(config,{webpack})=>{
         config.plugins.push(
             new webpack.DefinePlugin({

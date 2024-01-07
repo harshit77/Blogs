f5493cdadb29eb5c4aa11cd17ac2465ac525f8ce
Harshit Sahu <hsahu77@gmail.com>
2024-01-07 18:23:00 +0530

    Updated Response format as per netlify2.0

--- netlify/functions/schedule_message.ts
+++ netlify/functions/schedule_message.ts
@@ -4,9 +4,7 @@ import axios from "axios";
 const handler = async (req: Request, res: Response) => {
   console.log("Schedule kicked off");
   await axios.get("https://dashmind.netlify.app/api/scheduleMessages");
-  return {
-    statusCode: 200,
-  };
+  return new Response("Ok");
 };
 
 export const config: Config = {

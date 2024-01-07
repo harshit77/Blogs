c2ceaecf4f76d65bda9822b11f881272d5847410
Harshit Sahu <hsahu77@gmail.com>
2024-01-07 18:16:41 +0530

    Added DashbmindURL

--- netlify/functions/schedule_message.ts
+++ netlify/functions/schedule_message.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 
 const handler = async (req: Request, res: Response) => {
   console.log("Schedule kicked off");
-  await axios.get("/api/scheduleMessages");
+  await axios.get("https://dashmind.netlify.app/api/scheduleMessages");
   return {
     statusCode: 200,
   };
 };
 
 export const config: Config = {
-  schedule: "45 12 * * *",
+  schedule: "50 12 * * *",
 };
 
 export default handler;
